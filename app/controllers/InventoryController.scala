b6c43831df28d543b198e98bc2805dab3a1062e4
Lex Bedwell <LexBedwell@gmail.com>
2019-07-07 17:16:15 -0400

    Revert "test db query added"
    This reverts commit 9e5087a67d740f02d5a6975827af7d879705471b.

--- app/controllers/InventoryController.scala
+++ app/controllers/InventoryController.scala
@@ -8,7 +8,6 @@ class InventoryController extends InjectedController {
 
   def productInventory(productId: String) = Action { request =>
     val response: JsValue = JsObject(Seq("productId" -> JsNumber(productId.toInt), "inventory" -> JsString("???")))
-    db.Main.main(Array(""))
     Ok(response)
   }
 
