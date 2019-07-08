a4664f3e1935e6442a1f0d1dadaceaf336fdd8c1
Lex Bedwell <LexBedwell@gmail.com>
2019-07-07 23:25:09 -0400

    get route done

--- app/controllers/InventoryController.scala
+++ app/controllers/InventoryController.scala
@@ -4,21 +4,26 @@ import play.api.mvc._
 import play.api.libs.json._
 import javax.inject.Inject
 import db.InventoryDao
-import scala.concurrent.{ExecutionContext, Future, Await}
+import scala.concurrent.{ExecutionContext, Await}
 import scala.concurrent.duration._
 import akka.actor._
 
 
-class InventoryController @Inject()(cc: MessagesControllerComponents,
-                                    dao: InventoryDao,
-                                    system: ActorSystem
-                                   )(implicit ec: ExecutionContext)extends InjectedController {
+class InventoryController @Inject()(cc: MessagesControllerComponents, dao: InventoryDao, system: ActorSystem)(implicit ec: ExecutionContext)
+  extends InjectedController {
 
   def productInventory(productId: String) = Action { request =>
     val dbResponse = Await.result(dao.fetchInventory(productId), 5000 millis)
-    println(dbResponse)
-    val response: JsValue = JsObject(Seq("productId" -> JsNumber(productId.toInt), "inventory" -> JsString("???")))
-    Ok(response)
+    dbResponse.length match {
+      case 1 => {
+        val response: JsValue = JsObject(Seq("productId" -> JsNumber(dbResponse(0).id), "inventory" -> JsNumber(dbResponse(0).qty)))
+        Ok(response)
+      }
+      case _ => {
+        val response: JsValue = JsObject(Seq("error" -> JsString("unable to find productId")))
+        Ok(response)
+      }
+    }
   }
 
 }
