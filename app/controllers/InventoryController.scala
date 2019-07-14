62e9920ad568c375950bd0427d0450bcf4124287
Lex Bedwell <LexBedwell@gmail.com>
2019-07-14 15:36:00 -0400

    add tuple

--- app/controllers/InventoryController.scala
+++ app/controllers/InventoryController.scala
@@ -11,7 +11,9 @@ import scala.concurrent.duration._
 class InventoryController @Inject()(dao: InventoryDao) extends InjectedController {
 
   def getProductInventory(productId: String) = Action { request =>
+
     val dbResponse = Await.result(dao.fetchInventory(productId), 5000 millis)
+
     dbResponse.length match {
       case 1 => {
         val response: JsValue = JsObject(Seq("productId" -> JsNumber(dbResponse(0).id), "inventory" -> JsNumber(dbResponse(0).qty)))
@@ -22,26 +24,29 @@ class InventoryController @Inject()(dao: InventoryDao) extends InjectedControlle
         Ok(response)
       }
     }
+
   }
 
   def updateProductInventory = Action(parse.json) { request =>
-    val orderedInventoryMap = request.body.validate[Map[String,Int]].getOrElse(Map[String, Int]())
-    val orderedInventoryTouple = orderedInventoryMap.map{ case (productId, orderQty) => {
+
+    val orderedInventory = request.body.validate[Map[String,Int]].getOrElse(Map[String, Int]())
+
+    val orderedInventoryStatus = orderedInventory.map{ case (productId, orderQty) => {
+
       val dbResponse = Await.result(dao.fetchInventory(productId), 5000 millis)
+
       val isInStock = dbResponse.length match {
-        case 1 => { dbResponse(0).qty match {
-          case inv if (inv - orderQty > 0) => true
+        case 1 => dbResponse(0).qty match {
+          case inv if inv - orderQty > 0 => true
           case _ => false
           }
-        }
         case _ => false
       }
 
-      println("productId is: " + productId)
-      println("is isInStock is: " + isInStock)
-      println("*****")
+      new Tuple3(productId, orderQty, isInStock)
     }}
-    //println(orderedInventoryMap)
+
+    println(orderedInventoryStatus)
     Ok("Success!")
   }
 
